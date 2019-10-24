088bf787d5ca2c34401fd3ece9fc6a3100e890ba
Student <example@example.com>
2019-10-24 16:17:56 -0700

    add if statement related to age

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-$(document).ready(function()
-  $("#formOne").submit(function(event) {
-  var yourAge  = $("input#age").val()
-  var yourInterest = $("input#gender").val()
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
 
-  if 
+    var yourAge = parseInt($("input#age").val());
+    console.log(yourAge);
+    var yourInterest = $("input#gender").val();
+    console.log(yourInterest);
+
+
+    if (yourAge < 30) {
+      alert("end");
+      $("#youngmale").show();
+      $("#youngfemale").show();
+
+    }
+
+    });
+  });
